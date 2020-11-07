246b1b3090596b964109fd976ac1f96b5f74546f
Ved Joshi <50056400+Ved-Joshi@users.noreply.github.com>
2020-11-06 18:55:03 -0800

    Initial Commit

---
+++ cquery_cached_index/c@@users@ved-j@my pros project/.vscode@subsystemheaders@globals.hpp
@@ -0,0 +1,19 @@
+#include "main.h"
+
+//MOTORS
+
+extern pros::Motor lift;
+extern pros::Motor intakeLeft;
+extern pros::Motor intakeRight;
+extern pros::Motor driveFrontLeft;
+extern pros::Motor driveFrontRight;
+extern pros::Motor driveBackLeft;
+extern pros::Motor driveBackRight;
+
+//CONTROLLER
+
+extern pros::Controller controller;
+
+//MISCELLANEOUS
+
+//SENSORS
